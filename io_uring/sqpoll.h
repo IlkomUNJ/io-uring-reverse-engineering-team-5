34ba5c6e4cfc3386ffa657a4281686fe3ad9211c
intannk <147404999+intannk@users.noreply.github.com>
2025-05-14 20:58:12 +0700

    Update sqpoll.h

--- io_uring/sqpoll.h
+++ io_uring/sqpoll.h
@@ -20,12 +20,19 @@ struct io_sq_data {
 	unsigned long		state;
 	struct completion	exited;
 };
-
+//Menyiapkan thread SQPOLL untuk memindahkan pemrosesan submission queue dari user space ke kernel.
 int io_sq_offload_create(struct io_ring_ctx *ctx, struct io_uring_params *p);
+//Menjamin pembersihan thread SQPOLL dengan benar saat konteks dihancurkan.
 void io_sq_thread_finish(struct io_ring_ctx *ctx);
+//Mengakhiri thread SQPOLL secara bersih dan terkontrol.
 void io_sq_thread_stop(struct io_sq_data *sqd);
+//Menangguhkan thread SQPOLL untuk sementara waktu.
 void io_sq_thread_park(struct io_sq_data *sqd);
+//Melanjutkan kembali eksekusi thread SQPOLL yang sedang diparkir.
 void io_sq_thread_unpark(struct io_sq_data *sqd);
+//Mengelola siklus hidup dari struktur data SQPOLL secara otomatis dan aman.
 void io_put_sq_data(struct io_sq_data *sqd);
+//Menjamin bahwa submission queue tidak penuh sebelum entri baru ditambahkan.
 void io_sqpoll_wait_sq(struct io_ring_ctx *ctx);
+//Mengatur thread SQPOLL agar berjalan pada CPU yang ditentukan untuk efisiensi dan isolasi kinerja.
 int io_sqpoll_wq_cpu_affinity(struct io_ring_ctx *ctx, cpumask_var_t mask);
