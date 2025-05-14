4b507ad0a29d02f7b619d3d5f93e51271b120992
intannk <147404999+intannk@users.noreply.github.com>
2025-05-14 20:21:09 +0700

    Update register.h

--- io_uring/register.h
+++ io_uring/register.h
@@ -2,8 +2,28 @@
 #ifndef IORING_REGISTER_H
 #define IORING_REGISTER_H
 
+/*
+ * Menghapus eventfd dari konteks io_uring.
+ * Digunakan untuk membersihkan dan menghapus eventfd
+ * yang sebelumnya telah didaftarkan dalam instance io_uring.
+ */
 int io_eventfd_unregister(struct io_ring_ctx *ctx);
+
+
+/*
+ * Menghapus personality dari konteks io_uring berdasarkan ID-nya
+ * dan melepaskan kredensial yang terkait.
+ * Memastikan pembersihan personality yang telah didaftarkan.
+ */
 int io_unregister_personality(struct io_ring_ctx *ctx, unsigned id);
+
+/*
+ * Mengambil file berdasarkan file descriptor yang diberikan.
+ * Jika 'registered' bernilai true, file diambil dari ring yang
+ * telah didaftarkan; jika tidak, dari tabel file biasa.
+ * Mendukung akses ke file baik yang terdaftar maupun tidak
+ * dalam operasi io_uring.
+ */
 struct file *io_uring_register_get_file(unsigned int fd, bool registered);
 
 #endif
